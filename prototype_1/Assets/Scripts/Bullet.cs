b2770ee60043a5265fdb04648a1c19a38598833d
Unknown <paosalcedo@gmail.com>
2017-02-11 03:41:14 -0500

    Initial commit of "Split Fighter" prototype

---
+++ prototype_1/Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	AudioSource bang;
+
+	// Use this for initialization
+	void Start () {
+		bang = GetComponent<AudioSource> ();
+		bang.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Wall") {
+			Destroy (gameObject);
+		}
+	}
+
+
+}
